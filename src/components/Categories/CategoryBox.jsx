f67d1a548ebaf28c5f83b5125bf6d81fc14270cb
unknown <showlinakter77@gmail.com>
2024-09-17 20:29:13 +0600

    signUp page complete

--- src/components/Categories/CategoryBox.jsx
+++ src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,26 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get('category');
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // 1.create Query string
+    let currentQuery = { category: label };
+    const url = queryString.stringifyUrl({
+      url: '/',
+      query: currentQuery,
+    })
+    // console.log(url);
+    // 2. Set query string in url
+    navigate(url);
+  }
   return (
     <div
+      onClick={handleClick}
       className={`flex 
   flex-col 
   items-center 
@@ -12,7 +30,7 @@ const CategoryBox = ({ label, icon: Icon }) => {
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
+  cursor-pointer ${category === label && 'border-b-neutral-800 text-neutral-800'} `}
     >
       <Icon size={26} />
       <div className='text-sm font-medium'>{label}</div>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,19 +1,70 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../hooks/useAuth'
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { TbFidgetSpinner } from "react-icons/tb";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const { createUser, signInWithGoogle, updateUserProfile, loading, setLoading } = useAuth();
+
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const form = e.target
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    const image = form.image.files[0];
+    const formData = new FormData();
+    formData.append('image', image);
+    console.log(formData);
+    try {
+      setLoading(true);
+      // 1. Upload image and get image url
+      const { data } = await axios.post(
+        `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+        formData
+      )
+      console.log(data.data.display_url);
+
+      // 2. User Registration
+      const result = await createUser(email, password);
+      console.log(result);
+
+      //  3. Save username and photo in firebase
+      await updateUserProfile(name, data.data.display_url);
+      navigate('/');
+      toast.success('SignUp Successful')
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+
+  }
+
+  // handle google signin
+  const handleGoogleSignIn = async () => {
+    try {
+      await signInWithGoogle();
+      navigate('/');
+      toast.success('SignUp Successful')
+
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
+  }
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
         <div className='mb-8 text-center'>
-          <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
+          <h1 className='my-3 text-4xl font-bold '>Sign Up</h1>
           <p className='text-sm text-gray-400'>Welcome to StayVista</p>
         </div>
-        <form
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
-        >
+        <form onSubmit={handleSubmit} className='space-y-6'>
           <div className='space-y-4'>
             <div>
               <label htmlFor='email' className='block mb-2 text-sm'>
@@ -74,10 +125,11 @@ const SignUp = () => {
 
           <div>
             <button
+              disabled={loading}
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              Continue
+              {loading ? <TbFidgetSpinner className='animate-spin m-auto'></TbFidgetSpinner> : 'Continue'}
             </button>
           </div>
         </form>
@@ -88,11 +140,14 @@ const SignUp = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          disabled={loading}
+          onClick={handleGoogleSignIn}
+          className='disabled:cursor-not-allowed flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Already have an account?{' '}
           <Link
